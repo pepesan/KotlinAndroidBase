5104f2bc63cf2fb129d83d7d6abdd4b96158d1f5
David Vaquero <pepesan@gmail.com>
2018-05-08 09:28:25 +0200

    importación del textView para evitar el findViewById

--- app/src/main/java/com/cursosdedesarrollo/kotlinandroidbase/MainActivity.kt
+++ app/src/main/java/com/cursosdedesarrollo/kotlinandroidbase/MainActivity.kt
@@ -6,17 +6,22 @@ import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.TextView
 
+//importación del layout para usarlo directamente
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
 
 class MainActivity : AppCompatActivity() {
-    var textview: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        //ya no es necesario el textview=this.findViewById(R.id.textView)
+        // debido al import de arriba
+        textView.setText("Hola Mundo!!")
+        Log.d("app",textView?.toString())
         /*
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
